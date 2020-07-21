23a71d835f83718faa02c048d9300907628b6eca
Ali Ahmed <ali.ahmed@punchcard.io>
2020-07-20 18:15:19 -0600

    Animated button as a separate module

--- src/ButtonModule.elm
+++ src/ButtonModule.elm
@@ -1,17 +1,206 @@
-module ButtonModule exposing (..)
+module ButtonModule exposing (ButtonModel, ButtonMsg, animationMsg, buttonUpdater, createButton, initButtonModel, toHtml)
 
-type MyButton msg = MyButton (Options msg) String
+import Css exposing (..)
+import Css.Animations as CA exposing (keyframes)
+import Css.Transitions exposing (cubicBezier, transition)
+import Html
+import Html.Styled exposing (Attribute, Html, button, div, text, toUnstyled)
+import Html.Styled.Attributes exposing (css)
+import Html.Styled.Events exposing (onClick, onMouseUp)
+import Process
+import Task
+
+
+type MyButton msg
+    = MyButton (Options msg) String
+
 
 type alias Options msg =
-  { startAnimation : Bool
-  , onClick : Maybe msg
-	}
+    { startAnimation : Bool
+    , onClick : Maybe msg
+    , onMouseUp : Maybe msg
+    }
 
-createButton : String -> msg -> Button msg
-createButton str msg = Button defaultOptions str
+
+type ButtonMsg
+    = Animate
+    | Reset
+
+
+type ButtonModel
+    = ButtonModel { animate : Bool }
+
+
+initButtonModel : ButtonModel
+initButtonModel =
+    ButtonModel
+        { animate = False }
+
+
+createButton : String -> Maybe msg -> Maybe msg -> MyButton msg
+createButton str onClickMsg animateMsg =
+    MyButton { defaultOptions | onClick = onClickMsg, onMouseUp = animateMsg } str
+
+
+animationMsg : ButtonMsg
+animationMsg =
+    Animate
+
+
+toHtml : ButtonModel -> MyButton msg -> Html msg
+toHtml (ButtonModel model) (MyButton options label) =
+    let
+        styles =
+            buttonStyles model.animate
+    in
+    case ( options.onClick, options.onMouseUp ) of
+        ( Just onClickMsg, Just onMouseUpMsg ) ->
+            button [ onClick onClickMsg, onMouseUp onMouseUpMsg, css <| styles ] [ text label ]
+
+        ( Just onClickMsg, Nothing ) ->
+            button [ onClick onClickMsg, css <| styles ] [ text label ]
+
+        ( Nothing, Just onMouseUpMsg ) ->
+            button [ onMouseUp onMouseUpMsg, css <| styles ] [ text label ]
+
+        ( Nothing, Nothing ) ->
+            button [ css <| styles ] [ text label ]
+
+
+resetCommand : Cmd ButtonMsg
+resetCommand =
+    Process.sleep 1000.0
+        |> Task.andThen (always <| Task.succeed Reset)
+        |> Task.perform identity
+
+
+buttonUpdater : ButtonMsg -> ButtonModel -> ( ButtonModel, Cmd ButtonMsg )
+buttonUpdater msg (ButtonModel model) =
+    case msg of
+        Animate ->
+            ( ButtonModel { model | animate = True }, resetCommand )
+
+        Reset ->
+            ( ButtonModel { model | animate = False }, Cmd.none )
+
 
 defaultOptions : Options msg
 defaultOptions =
-  { startAnimation : True
-  , onClick : Nothing
-  }
+    { startAnimation = True
+    , onClick = Nothing
+    , onMouseUp = Nothing
+    }
+
+
+buttonStyles : Bool -> List Style
+buttonStyles animate =
+    let
+        baseAttributes =
+            [ borderRadius (px 2)
+            , padding2 (px 4) (px 15)
+            , borderWidth (px 1)
+            , fontSize (px 14)
+            , height (px 34)
+            , outline none
+            ]
+
+        defaultButtonAttributes =
+            [ position relative
+            , color (hex primaryColor)
+            , borderStyle solid
+            , backgroundColor (hex "#fff")
+            , borderColor <| rgb 217 217 217
+            , focus
+                [ borderColor (hex primaryColorFaded)
+                , color (hex primaryColorFaded)
+                ]
+            , hover
+                [ borderColor (hex primaryColorFaded)
+                , color (hex primaryColorFaded)
+                ]
+            , active
+                [ borderColor (hex primaryColor)
+                , color (hex primaryColor)
+                ]
+            , transition
+                [ Css.Transitions.borderColor transitionDuration
+                , Css.Transitions.color transitionDuration
+                ]
+            ]
+    in
+    if animate == False then
+        baseAttributes ++ defaultButtonAttributes
+
+    else
+        baseAttributes
+            ++ defaultButtonAttributes
+            ++ [ position relative
+               , borderColor (hex primaryColor)
+               , color (hex primaryColor)
+               , animatedBefore
+               ]
+
+
+animatedBefore : Style
+animatedBefore =
+    before
+        [ property "content" "\" \""
+        , display block
+        , position absolute
+        , width (pct 100)
+        , height (pct 100)
+        , right (px 0)
+        , left (px 0)
+        , top (px 0)
+        , bottom (px 0)
+        , borderRadius (px 5)
+        , backgroundColor <| hex primaryColor
+        , transition [ clickTransition ]
+        , zIndex (int -1)
+        , animationName animation
+        , animationDuration (sec 2)
+        , property "animation-timing-function" "cubic-bezier(0.08, 0.82, 0.17, 1)"
+        , property "animation-fill-mode" "forwards"
+        , animationIterationCount (int 1)
+        ]
+
+
+clickTransition =
+    Css.Transitions.transform3 2000 2000 <| cubicBezier 0.08 0.82 0.17 1
+
+
+primaryColor : String
+primaryColor =
+    "#1890ff"
+
+
+primaryColorFaded : String
+primaryColorFaded =
+    "#40a9ff"
+
+
+primaryColorStrong : String
+primaryColorStrong =
+    "#096dd9"
+
+
+warningColor : String
+warningColor =
+    "#faad14"
+
+
+dangerColor : String
+dangerColor =
+    "#ff4d4f"
+
+
+animation =
+    keyframes
+        [ ( 50, [ CA.property "transform" "scale(1.1, 1.3)", CA.property "opacity" "0" ] )
+        , ( 99, [ CA.property "transform" "scale(0.001, 0.001)", CA.property "opacity" "0" ] )
+        , ( 100, [ CA.property "transform" "scale(0.001, 0.001)", CA.property "opacity" "1" ] )
+        ]
+
+
+transitionDuration =
+    350
--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,12 @@
 module Main exposing (..)
 
 import Browser
+import ButtonModule exposing (ButtonModel, ButtonMsg, animationMsg, buttonUpdater, createButton, initButtonModel, toHtml)
 import Css exposing (..)
-import Css.Animations as CA exposing (keyframes)
-import Css.Transitions exposing (cubicBezier, transition)
 import Html
 import Html.Styled exposing (Attribute, Html, button, div, text, toUnstyled)
 import Html.Styled.Attributes exposing (css)
 import Html.Styled.Events exposing (onClick, onMouseUp)
-import Process
-import Task
 
 
 
@@ -18,13 +15,14 @@ import Task
 
 type alias Model =
     { value : String
-    , animate : Bool
+    , buttonModel : ButtonModel
+    , buttonModel2 : ButtonModel
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { value = "", animate = False }, Cmd.none )
+    ( { value = "", buttonModel = initButtonModel, buttonModel2 = initButtonModel }, Cmd.none )
 
 
 
@@ -32,22 +30,18 @@ init =
 
 
 type Msg
-    = ButtonClicked
-    | Animate
-    | Reset
+    = ButtonMsg ButtonMsg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ButtonClicked ->
-            ( { model | value = "Button Clicked" }, resetCommand )
-
-        Animate ->
-            ( { model | animate = True }, Cmd.none )
-
-        Reset ->
-            ( { model | animate = False }, Cmd.none )
+        ButtonMsg btnMsg ->
+            let
+                ( newButtonModel, btnCommands ) =
+                    buttonUpdater btnMsg model.buttonModel
+            in
+            ( { model | buttonModel = newButtonModel }, Cmd.map ButtonMsg btnCommands )
 
 
 
@@ -61,103 +55,10 @@ view model =
             [ css <|
                 [ position relative ]
             ]
-            [ button (buttonAttr model.animate) [ text "Click Me" ]
-            , text model.value
+            [ toHtml model.buttonModel (createButton "Click me" Nothing (Just <| ButtonMsg animationMsg))
+            , toHtml model.buttonModel2 (createButton "Click Me 2" Nothing Nothing)
             ]
         ]
-
-
-buttonAttr : Bool -> List (Attribute Msg)
-buttonAttr animate =
-    [ onClick ButtonClicked, onMouseUp Animate, css <| buttonStyles animate ]
-
-
-transitionDuration =
-    350
-
-
-buttonStyles : Bool -> List Style
-buttonStyles animate =
-    let
-        baseAttributes =
-            [ borderRadius (px 2)
-            , padding2 (px 4) (px 15)
-            , borderWidth (px 1)
-            , fontSize (px 14)
-            , height (px 34)
-            , outline none
-            ]
-
-        defaultButtonAttributes =
-            [ position relative
-            , color (hex primaryColor)
-            , borderStyle solid
-            , backgroundColor (hex "#fff")
-            , borderColor <| rgb 217 217 217
-            , focus
-                [ borderColor (hex primaryColorFaded)
-                , color (hex primaryColorFaded)
-                ]
-            , hover
-                [ borderColor (hex primaryColorFaded)
-                , color (hex primaryColorFaded)
-                ]
-            , active
-                [ borderColor (hex primaryColor)
-                , color (hex primaryColor)
-                ]
-            , transition
-                [ Css.Transitions.borderColor transitionDuration
-                , Css.Transitions.color transitionDuration
-                ]
-            ]
-    in
-    if animate == False then
-        baseAttributes ++ defaultButtonAttributes
-
-    else
-        baseAttributes
-            ++ defaultButtonAttributes
-            ++ [ position relative
-               , borderColor (hex primaryColor)
-               , color (hex primaryColor)
-               , animatedBefore
-               ]
-
-
-animatedBefore : Style
-animatedBefore =
-    before
-        [ property "content" "\" \""
-        , display block
-        , position absolute
-        , width (pct 100)
-        , height (pct 100)
-        , right (px 0)
-        , left (px 0)
-        , top (px 0)
-        , bottom (px 0)
-        , borderRadius (px 5)
-        , backgroundColor <| hex primaryColor
-        , transition [ clickTransition ]
-        , zIndex (int -1)
-        , animationName animation
-        , animationDuration (sec 2)
-        , property "animation-timing-function" "cubic-bezier(0.08, 0.82, 0.17, 1)"
-        , property "animation-fill-mode" "forwards"
-        , animationIterationCount (int 1)
-        ]
-
-
-clickTransition =
-    Css.Transitions.transform3 2000 2000 <| cubicBezier 0.08 0.82 0.17 1
-
-
-resetCommand : Cmd Msg
-resetCommand =
-    Process.sleep 1000.0
-        |> Task.andThen (always <| Task.succeed Reset)
-        |> Task.perform identity
 
 
 
@@ -172,50 +73,3 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
-
-
-primaryColor : String
-primaryColor =
-    "#1890ff"
-
-
-primaryColorFaded : String
-primaryColorFaded =
-    "#40a9ff"
-
-
-primaryColorStrong : String
-primaryColorStrong =
-    "#096dd9"
-
-
-warningColor : String
-warningColor =
-    "#faad14"
-
-
-dangerColor : String
-dangerColor =
-    "#ff4d4f"
-
-
-
--- @keyframes waveEffect {
---   100% {
---     box-shadow: 0 0 0 @primary-color;
---     box-shadow: 0 0 0 @wave-animation-width var(--antd-wave-shadow-color);
---   }
--- }
--- @keyframes fadeEffect {
---   100% {
---     opacity: 0;
---   }
--- }
-
-
-animation =
-    keyframes
-        [ ( 50, [ CA.property "transform" "scale(1.1, 1.3)", CA.property "opacity" "0" ] )
-        , ( 99, [ CA.property "transform" "scale(0.001, 0.001)", CA.property "opacity" "0" ] )
-        , ( 100, [ CA.property "transform" "scale(0.001, 0.001)", CA.property "opacity" "1" ] )
-        ]
